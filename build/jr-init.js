732372cd97cbb047e3910ea7cea9bb8929cf3b0d
hzxiongxu <xx@xiongxudeMacBook-Pro.local>
2018-01-08 17:42:44 +0800

    bug fix

--- build/jr-init.js
+++ build/jr-init.js
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
 
-var clone = require("git-clone");
-var commander = require("commander");
-var fsExtra = require("fs-extra");
-var fs = require("fs");
-var path = require("path");
-var chalk = require("chalk");
-var inquirer = require("inquirer");
+var clone = require('git-clone');
+var commander = require('commander');
+var fsExtra = require('fs-extra');
+var fs = require('fs');
+var path = require('path');
+var chalk = require('chalk');
+var inquirer = require('inquirer');
+var exec = require('child_process').exec
 
 /**
  * Help.
  */
 
-commander.on("--help", function() {
-  console.log("  Examples:");
+commander.on('--help', function() {
+  console.log('  Examples:');
   console.log();
-  console.log(chalk.gray("    # init a new project"));
-  console.log("    $ jr init my-project");
+  console.log(chalk.gray('    # init a new project'));
+  console.log('    $ jr init my-project');
   console.log();
 });
 
@@ -32,107 +33,128 @@ function help() {
 /**
  * 问答配置
  */
- function answers(flag){
-      inquirer
-        .prompt( [{
-            type: "input",
-            name: "projectName",
-            validate(input) {
-              var done = this.async();
-              setTimeout(function() {
-                if (input === "") {
-                  done("请先输入项目名称");
-                  return;
-                }
-                done(null, true);
-              }, 100);
-            },
-            message: "请输入项目名称(会作为网页标题):"
-          },
-          {
-            type: "input",
-            name: "author",
-            default: "hzxiongxu",
-            message: "请输入作者名称:"
-          },
-          {
-            type: "input",
-            name: "version",
-            default: "0.0.1",
-            message: "请输入版本号:"
-          }])
-        .then(ans => {
-          if(flag){
-             fsExtra.removeSync(folder)
-             console.log(chalk.green("删除目录'" + folder + "' 成功\n"));
-          }
-         getTemplate(ans)
-        });
-    }
+function answers(flag) {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'projectName',
+        validate(input) {
+          var done = this.async();
+          setTimeout(function() {
+            if (input === '') {
+              done('请先输入项目名称');
+              return;
+            }
+            done(null, true);
+          }, 100);
+        },
+        message: '请输入项目名称(会作为网页标题):',
+      },
+      {
+        type: 'input',
+        name: 'author',
+        default: 'hzxiongxu',
+        message: '请输入作者名称:',
+      },
+      {
+        type: 'input',
+        name: 'version',
+        default: '0.0.1',
+        message: '请输入版本号:',
+      },
+      {
+        type: 'rawlist',
+        name: 'style',
+        default: '蓝色',
+        choices: ['蓝色', '橙色', '红色'],
+        message: '请选择样式:',
+        validate(input) {
+          var done = this.async();
+          setTimeout(function() {
+            if (['蓝色', '橙色', '红色'].indexOf(input) === -1) {
+              done('请选择一种样式');
+              return;
+            }
+            done(null, true);
+          }, 100);
+        },
+      },
+    ])
+    .then(ans => {
+      if (flag) {
+        fsExtra.removeSync(folder);
+        console.log(chalk.green("删除目录'" + folder + "' 成功\n"));
+      }
+      getTemplate(ans);
+    });
+}
 
 /**
  * 模板下载更新
  */
- function getTemplate(ans){
-      fs.mkdirSync(folder)
-      console.log(chalk.green("新建目录'" + folder + "' 成功\n"));
-      console.log(chalk.blue("开始下载模板"));
-      setTimeout(() => {
-        console.log(chalk.blue("模板下载中,请稍后"));
-      }, 100);
-      clone("https://github.com/xxyj/regular-template", folder, {}, function(
-        err
-      ) {
+function getTemplate(ans) {
+  fs.mkdirSync(folder);
+  console.log(chalk.green("新建目录'" + folder + "' 成功\n"));
+  console.log(chalk.blue('开始下载模板'));
+  setTimeout(() => {
+    console.log(chalk.blue('模板下载中,请稍后'));
+  }, 100);
+  clone('https://github.com/xxyj/regular-template', folder, {}, function(err) {
+    if (err) {
+      console.log(chalk.red('下载模板失败 : ' + err.message.trim()));
+      return;
+    }
+    console.log(chalk.green('模板下载成功'));
+    console.log(chalk.blue('更新模板数据'));
+    // 删除git的关联
+    exec(`rm -rf ${folder}/.git`);
+    var package = folder + '/package.json';
+    if (!fs.existsSync(package)) {
+      console.red(chalk.blue('缺少package.json，更新失败'));
+      return;
+    }
+    fs.readFile(package, { flag: 'r+', encoding: 'utf8' }, (err, data) => {
+      if (err) {
+        console.log(chalk.red(err));
+        return;
+      }
+      var data = JSON.parse(data);
+      data.description = ans.projectName;
+      data.author = ans.author;
+      data.version = ans.version;
+      data.style = ans.style;
+      data = JSON.stringify(data, null, '\t');
+      fs.writeFile(package, data, err => {
         if (err) {
-          console.log(chalk.red("下载模板失败 : " + err.message.trim()));
+          console.log(chalk.red(err));
           return;
         }
-        console.log(chalk.green("模板下载成功"));
-        console.log(chalk.blue("更新模板数据"));
-        var package = folder + "/package.json"
-        if (!fs.existsSync(package)) {
-          console.red(chalk.blue("缺少package.json，更新失败"));
-          return;
-        }
-        fs.readFile(package, {flag: 'r+', encoding: 'utf8'}, (err, data) => {
-            if(err){
-                  console.log(chalk.red(err));
-                  return;
-              }
-              var  data= JSON.parse(data);
-              data.description = ans.projectName
-              data.author = ans.author
-              data.version = ans.version
-              data = JSON.stringify(data,null, "\t");
-              fs.writeFile(package, data, (err) => {
-                  if(err){
-                      console.log(chalk.red(err));
-                      return;
-                  }
-                  console.log(chalk.green("模板更新成功"));
-              })
-        });
+        console.log(chalk.green('模板更新成功'));
       });
- }
+    });
+  });
+}
 help();
-var  folder = commander.args[0];
+var folder = commander.args[0];
 /**
  * 开始配置模板
- */ 
+ */
+
 if (fs.existsSync(folder)) {
-    inquirer.prompt([{
-        type: "confirm",
-        name:"cover",
-        message: "目录已存在，是否覆盖？:"
-    }])
+  inquirer
+    .prompt([
+      {
+        type: 'confirm',
+        name: 'cover',
+        message: '目录已存在，是否覆盖？:',
+      },
+    ])
     .then(ans => {
-        if(ans.cover){
-          answers(true);
-        }
-    })
-}else{
-    answers();
+      if (ans.cover) {
+        answers(true);
+      }
+    });
+} else {
+  answers();
 }
-
-
-   
